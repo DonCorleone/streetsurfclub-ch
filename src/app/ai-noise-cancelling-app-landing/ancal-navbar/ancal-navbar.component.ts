34e01b421a1e4805a301150acc7867e8b42c3519
Linus Wieland <linus@wieland.click>
2024-03-05 21:22:50 +0100

    dark mode service

--- src/app/ai-noise-cancelling-app-landing/ancal-navbar/ancal-navbar.component.ts
+++ src/app/ai-noise-cancelling-app-landing/ancal-navbar/ancal-navbar.component.ts
@@ -1,65 +1,80 @@
-import { Component, HostListener } from '@angular/core';
-import { NgClass, NgIf } from '@angular/common';
+import {ChangeDetectionStrategy, Component, HostListener} from '@angular/core';
+import {AsyncPipe, NgClass, NgIf} from '@angular/common';
+import {DarkmodeService} from "../../services/darkmode.service";
 
 @Component({
-    selector: 'app-ancal-navbar',
-    templateUrl: './ancal-navbar.component.html',
-    styleUrls: ['./ancal-navbar.component.scss'],
-    standalone: true,
-    imports: [NgClass, NgIf]
+  selector: 'app-ancal-navbar',
+  templateUrl: './ancal-navbar.component.html',
+  styleUrls: ['./ancal-navbar.component.scss'],
+  standalone: true,
+  imports: [NgClass, NgIf, AsyncPipe],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AncalNavbarComponent {
 
-    // Active toggleClass
-    classApplied = false;
-    toggleClass() {
-        this.classApplied = !this.classApplied;
-    }
+  isDarkMode$ = this.darkmodeService.isDarkMode$;
 
-    // Section to Section Scroll
-    activeSection: string | null = null;
-    scrollTo(sectionId: string): void {
-        const element = document.getElementById(sectionId);
-        if (element) {
-            element.scrollIntoView({ behavior: 'smooth' });
-        }
-    }
+  constructor(private darkmodeService: DarkmodeService) {
+  }
 
-    // Navbar Sticky
-    isSticky: boolean = false;
-    @HostListener('window:scroll', ['$event'])
-    checkScroll() {
-        const scrollPosition = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
-        if (scrollPosition >= 50) {
-            this.isSticky = true;
-        } else {
-            this.isSticky = false;
-        }
-    }
+  // Active toggleClass
+  classApplied = false;
 
-    // Contact Popup
-    isOpen = false;
-    openPopup(): void {
-        this.isOpen = true;
-    }
-    closePopup(): void {
-        this.isOpen = false;
-    }
+  toggleClass() {
+    this.classApplied = !this.classApplied;
+  }
 
-    // Signin/Signup Popup
-    isSigninSignupOpen = false;
-    openSigninSignupPopup(): void {
-        this.isSigninSignupOpen = true;
-    }
-    closeSigninSignupPopup(): void {
-        this.isSigninSignupOpen = false;
-    }
+  // Section to Section Scroll
+  activeSection: string | null = null;
 
-    // Tabs
-    currentTab = 'tab1';
-    switchTab(event: MouseEvent, tab: string) {
-        event.preventDefault();
-        this.currentTab = tab;
+  scrollTo(sectionId: string): void {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({behavior: 'smooth'});
     }
+  }
+
+  // Navbar Sticky
+  isSticky: boolean = false;
+
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    const scrollPosition = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    if (scrollPosition >= 50) {
+      this.isSticky = true;
+    } else {
+      this.isSticky = false;
+    }
+  }
+
+  // Contact Popup
+  isOpen = false;
+
+  openPopup(): void {
+    this.isOpen = true;
+  }
+
+  closePopup(): void {
+    this.isOpen = false;
+  }
+
+  // Signin/Signup Popup
+  isSigninSignupOpen = false;
+
+  openSigninSignupPopup(): void {
+    this.isSigninSignupOpen = true;
+  }
+
+  closeSigninSignupPopup(): void {
+    this.isSigninSignupOpen = false;
+  }
+
+  // Tabs
+  currentTab = 'tab1';
+
+  switchTab(event: MouseEvent, tab: string) {
+    event.preventDefault();
+    this.currentTab = tab;
+  }
 
 }
