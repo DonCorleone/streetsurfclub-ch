a46d9b8c8438942f0df798184134dfc63c7b5536
Linus Wieland <linus@wieland.click>
2024-03-30 23:35:29 +0100

    two images

--- src/app/blog-details-page/blog-details-page.component.ts
+++ src/app/blog-details-page/blog-details-page.component.ts
@@ -30,18 +30,40 @@ export class BlogDetailsPageComponent implements OnInit {
             // Do something with the blog details
             let decodedContent = decodeURIComponent(page.content.replace(/\\u/g, '%'));
             console.log(decodedContent);
-            let regex = /(<div>\s*<div style="text-align: center;?"?>\s*<a href="[^"]*">\s*<img[^>]*><\/a>\s*<\/div>\s*<br \/><b><br \/><\/b>\s*<\/div>)/;
-            let match = decodedContent.match(regex);
+            let regexImage = /(<div>\s*<div style="text-align: center;?"?>\s*<a href="[^"]*">\s*<img[^>]*><\/a>\s*<\/div>\s*<br \/><b><br \/><\/b>\s*<\/div>)/;
+            let match = decodedContent.match(regexImage);
             let imgBlock = match ? match[1] : null;
             
 
-            this.content = decodedContent.replace(regex, '');
+            decodedContent = decodedContent.replace(regexImage, '');
 
             if (imgBlock) {
               let srcRegex = /<img[^>]+src="([^">]+)"/;
               let srcMatch = imgBlock.match(srcRegex);
               this.headerImg = srcMatch ? srcMatch[1] : null;
             }
+
+            console.log(decodedContent);
+            let regexTwoImages = /(<div class="separator" style="clear: both;">\s*<a href="([^"]*)"[^>]*>[\s\S]*?<\/div>\s*<div class="separator" style="clear: both;">\s*<a href="([^"]*)"[^>]*>[\s\S]*?<\/div>)/;
+            let matchTwoImages = decodedContent.match(regexTwoImages);
+            let imgBlockTwoImages = matchTwoImages ? matchTwoImages[1] : null;
+            
+            if (imgBlockTwoImages) {
+              let replacement = `
+              <div class="grid grid-cols-1 md:grid-cols-2 self-center gap-[25px] my-[20px] md:my-[25px] xl:my-[35px]">
+                <div class="text-center">
+                  <img src="${matchTwoImages ? matchTwoImages[2]: ''}" class="rounded-t-[20px] rounded-bl-[20px] rounded-br-[20px] md:rounded-br-[70px] lg:rounded-br-[90px]" alt="blog-details-image">
+                </div>
+                <div class="text-center">
+                  <img src="${matchTwoImages ? matchTwoImages[3] : ''}" class="rounded-b-[20px] rounded-tr-[20px] rounded-tl-[20px] md:rounded-tl-[90px] lg:rounded-tl-[70px]" alt="blog-details-image">
+                </div>
+              </div>
+              `;
+            
+              decodedContent = decodedContent.replace(regexTwoImages, replacement);
+            }
+
+            this.content = decodedContent;
           }
         })
       )
