312816476ae7a1d422397e3c251487df1c654260
Linus Wieland <linus@wieland.click>
2024-04-01 21:44:05 +0200

    new endpoint

--- src/app/services/blogger.service.ts
+++ src/app/services/blogger.service.ts
@@ -47,7 +47,7 @@ export class BloggerService {
       );
     }else{
       console.log('Production Mode');
-      return this.httpClient.get<Post>('.netlify/functions/get-post?postId=' + postId);
+      return this.httpClient.get<Post>('/get-post?postid=' + postId);
     }
   }
 
@@ -63,7 +63,7 @@ export class BloggerService {
       );
     }else{
       console.log('Production Mode');
-      return this.httpClient.get<Page>('.netlify/functions/get-page?pageId=' + pageId);
+      return this.httpClient.get<Page>('/get-page?pageid=' + pageId);
     }
   }
 
